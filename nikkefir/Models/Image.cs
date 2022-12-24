d815959b340d2be4a34df621b8f1119ea42bb7fe
kanadeiar <kanadeiar@gmail.com>
2022-12-24 03:19:48 +0300

    Чистка

--- nikkefir/Models/Image.cs
+++
@@ -1,9 +0,0 @@
-namespace nikkefir.Models;
-
-public class Image
-{
-    public int Id { get; set; }
-    public DateTime Date { get; set; }
-    public string? Name { get; set; }
-    public string? Path { get; set; }
-}
