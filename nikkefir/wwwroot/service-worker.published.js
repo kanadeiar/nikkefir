20c1d8b1a1dcf5a76aaba56e2da03005d7e78a61
kanadeiar <kanadeiar@gmail.com>
2023-01-04 20:44:32 +0300

    Изменения для публикации

---
+++ nikkefir/wwwroot/service-worker.published.js
@@ -0,0 +1,47 @@
+self.importScripts('./service-worker-assets.js');
+self.addEventListener('install', event => event.waitUntil(onInstall(event)));
+self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
+
+const cacheNamePrefix = 'offline-cache-';
+const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}`;
+const offlineAssetsInclude = [/\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/, /\.blat$/, /\.dat$/];
+const offlineAssetsExclude = [/^service-worker\.js$/];
+
+async function onInstall(event) {
+    console.info('Service worker: Install');
+}
+
+async function onActivate(event) {
+    console.info('Service worker: Activate');
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== cacheName) {
+                        console.log('Service Worker: Clearing Old Cache');
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+}
+
+self.addEventListener('fetch', e => {
+    console.log('Service Worker: Fetching');
+    e.respondWith(
+        fetch(e.request)
+            .then(res => {
+                // Make copy/clone of response
+                const resClone = res.clone();
+                // Open cahce
+                caches.open(cacheName).then(cache => {
+                    // Add response to cache
+                    cache.put(e.request, resClone);
+                });
+                return res;
+            })
+            .catch(err => caches.match(e.request).then(res => res))
+    );
+});
+
